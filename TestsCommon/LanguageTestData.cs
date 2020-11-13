dd322a668875643ce375976cc3787f1d6a74ee44
Mustafa Zengin <muzengin@microsoft.com>
2020-11-12 22:24:13 -0800

    use c#9 features

--- TestsCommon/LanguageTestData.cs
+++ TestsCommon/LanguageTestData.cs
@@ -2,48 +2,48 @@
 
 namespace TestsCommon
 {
-    public class LanguageTestData
+    public record LanguageTestData
     {
         /// <summary>
         /// Docs version e.g. V1 or Beta
         /// </summary>
-        public Versions Version { get; set; }
+        public Versions Version { get; init; }
 
         /// <summary>
         /// Whether the test case is failing due to a known issue
         /// </summary>
-        public bool IsKnownIssue { get; set; }
+        public bool IsKnownIssue { get; init; }
 
         /// <summary>
         /// Message to represent known issue
         /// </summary>
-        public string KnownIssueMessage { get; set; }
+        public string KnownIssueMessage { get; init; }
 
         /// <summary>
         /// Documentation link where snippet is shown
         /// </summary>
-        public string DocsLink { get; set; }
+        public string DocsLink { get; init; }
 
         /// <summary>
         /// Snippet file name
         /// </summary>
-        public string FileName { get; set; }
+        public string FileName { get; init; }
 
         /// <summary>
         /// Optional dll path to load Microsoft.Graph from a local resource instead of published nuget
         /// </summary>
-        public string DllPath { get; set; }
+        public string DllPath { get; init; }
         /// <summary>
         /// Optional. Version to use for the java core library. Ignored when using JavaPreviewLibPath
         /// </summary>
-        public string JavaCoreVersion { get; set; }
+        public string JavaCoreVersion { get; init; }
         /// <summary>
         /// Optional. Version to use for the java service library. Ignored when using JavaPreviewLibPath
         /// </summary>
-        public string JavaLibVersion { get; set; }
+        public string JavaLibVersion { get; init; }
         /// <summary>
         /// Optional. Folder container the java core and java service library repositories so the unit testing uses that local version instead.
         /// </summary>
-        public string JavaPreviewLibPath { get; set; }
+        public string JavaPreviewLibPath { get; init; }
     }
 }
