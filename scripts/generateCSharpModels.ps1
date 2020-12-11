b4e489e66d77ea3c6f99cb6b3222b70db1c8c9c8
Vincent Biret <vibiret@microsoft.com>
2020-12-11 08:34:20 -0500

    - fixes references to system environement variables

--- scripts/generateCSharpModels.ps1
+++ scripts/generateCSharpModels.ps1
@@ -1,10 +1,10 @@
 # Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 
 # set output and typewriter executable path
-$outputPath = Join-Path $env:Build_SourcesDirectory "output"
+$outputPath = Join-Path $env:BUILD_SOURCESDIRECTORY "output"
 Write-Host "Path to typewriter.exe output $outputPath"
 
-$typewriterPath = (Join-Path (Join-Path $env:Build_SourcesDirectory\MSGraph-SDK-Code-Generator\src\Typewriter\bin\ $env:BuildConfiguration) Typewriter.exe)
+$typewriterPath = (Join-Path (Join-Path $env:BUILD_SOURCESDIRECTORY/MSGraph-SDK-Code-Generator/src/Typewriter/bin/ $env:BuildConfiguration) Typewriter.exe)
 Write-Host "Path to typewriter tool: $typewriterPath"
 
 # download metadata
@@ -15,8 +15,8 @@ Invoke-WebRequest -Uri $env:MetadataURL -OutFile $metadataFile
 Write-Host "Downloaded $env:MetadataURL to $metadataFile" -ForegroundColor Green
 
 # transform metadata
-$xslTranformScriptPath = "$env:Build_SourcesDirectory\msgraph-metadata\transforms\csdl\transform.ps1"
-$xslPath = "msgraph-metadata\transforms\csdl\preprocess_csdl.xsl"
+$xslTranformScriptPath = "$env:BUILD_SOURCESDIRECTORY/msgraph-metadata/transforms/csdl/transform.ps1"
+$xslPath = "msgraph-metadata/transforms/csdl/preprocess_csdl.xsl"
 $cleanMetadataFile = Join-Path $metadataDir "clean_metadata.xml"
 
 # transform script uses relative path to working directory
@@ -28,7 +28,7 @@ Write-Host "Running Typewriter..."
 & $typewriterPath -v Info -m $cleanMetadataFile -o $outputPath -g Files
 
 # set repo models directory
-$repoModelsDir = "$env:Build_SourcesDirectory\msgraph-sdk-dotnet\src\Microsoft.Graph\Generated\"
+$repoModelsDir = "$env:BUILD_SOURCESDIRECTORY/msgraph-sdk-dotnet/src/Microsoft.Graph/Generated/"
 Write-Host "Path to repo models directory: $repoModelsDir"
 
 # clean old models
@@ -36,6 +36,6 @@ Remove-Item -Recurse $repoModelsDir | Write-Host
 Write-Host "Removed the existing generated files in the repo." -ForegroundColor Green
 
 # copy new models
-$modelsDirectory = Join-Path $outputPath "\com\microsoft\graph\"
+$modelsDirectory = Join-Path $outputPath "/com/microsoft/graph/"
 Move-Item $modelsDirectory $repoModelsDir
 Write-Host "Moved the models from $modelsDirectory into the local repo." -ForegroundColor Green
