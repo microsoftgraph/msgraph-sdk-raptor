ff216b9f61f4daf8d8911355045f445df4e3f33f
Vincent Biret <vibiret@microsoft.com>
2020-12-11 09:15:07 -0500

    - better path management for typewriter run

--- scripts/generateCSharpModels.ps1
+++ scripts/generateCSharpModels.ps1
@@ -4,11 +4,12 @@
 $outputPath = Join-Path $env:BUILD_SOURCESDIRECTORY "output"
 Write-Host "Path to typewriter output $outputPath"
 
-$typewriterPath = Join-Path $env:BUILD_SOURCESDIRECTORY "MSGraph-SDK-Code-Generator" "src" "Typewriter" "bin" $env:BuildConfiguration "net5.0" "Typewriter"
+$typewriterDirectory = Join-Path $env:BUILD_SOURCESDIRECTORY "MSGraph-SDK-Code-Generator" "src" "Typewriter" "bin" $env:BuildConfiguration "net5.0"
+$typewriterPath = Join-Path $typewriterDirectory "Typewriter"
 Write-Host "Path to typewriter tool: $typewriterPath"
 
 # download metadata
-$metadataDir = "metadata"
+$metadataDir = Join-Path $env:AGENT_TEMPDIRECTORY "metadata"
 mkdir $metadataDir
 $metadataFile = Join-Path $metadataDir "metadata.xml"
 Invoke-WebRequest -Uri $env:MetadataURL -OutFile $metadataFile
@@ -16,7 +17,7 @@ Write-Host "Downloaded $env:MetadataURL to $metadataFile" -ForegroundColor Green
 
 # transform metadata
 $xslTranformScriptPath = "$env:BUILD_SOURCESDIRECTORY/msgraph-metadata/transforms/csdl/transform.ps1"
-$xslPath = "msgraph-metadata/transforms/csdl/preprocess_csdl.xsl"
+$xslPath = "$env:BUILD_SOURCESDIRECTORY/msgraph-metadata/transforms/csdl/preprocess_csdl.xsl"
 $cleanMetadataFile = Join-Path $metadataDir "clean_metadata.xml"
 
 # transform script uses relative path to working directory
@@ -25,7 +26,9 @@ Write-Host "Transformed $metadataFile to $cleanMetadataFile" -ForegroundColor Gr
 
 # run Typewriter
 Write-Host "Running Typewriter..."
+Push-Location -Path $typewriterDirectory
 & $typewriterPath -v Info -m $cleanMetadataFile -o $outputPath -g Files
+Pop-Location
 
 # set repo models directory
 $repoModelsDir = "$env:BUILD_SOURCESDIRECTORY/msgraph-sdk-dotnet/src/Microsoft.Graph/Generated/"
