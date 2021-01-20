ddc7d121a4e216e4c259e8b88413348a2f5a4ca6
Vincent Biret <vibiret@microsoft.com>
2021-01-20 09:49:09 -0500

    - updates replacement script to account for separate files in java build

--- azure-pipelines/java-templates/replace-core-reference.ps1
+++ azure-pipelines/java-templates/replace-core-reference.ps1
@@ -1,5 +1,18 @@
-$content = Get-Content build.gradle -Raw;
-$content = $content -replace "api 'com\.microsoft\.graph:microsoft-graph-core:\d\.\d\.\d'", "implementation name: 'msgraph-sdk-java-core'";
+$buildFilePath = "build.gradle";
+$dependenciesFilePath = "gradle/dependencies.gradle";
+$buildAndDependencyFilesDistinct = (Test-Path -Path $dependenciesFilePath);
+if($buildAndDependencyFilesDistinct -eq $false) {
+    $dependenciesFilePath = $buildFilePath
+} 
+$buildContent = Get-Content -Path $buildFilePath -Raw;
+$dependenciesContent = Get-Content -Path $dependenciesFilePath -Raw;
+$dependenciesContent = $dependenciesContent -replace "api 'com\.microsoft\.graph:microsoft-graph-core:\d\.\d\.\d(?:-SNAPSHOT)?'", "implementation name: 'msgraph-sdk-java-core'";
 $flatDirRef = "mavenCentral()`r`n    flatDir {`r`n        dirs '$Env:CORE_PATH/build/libs'`r`n    }"
-$content = $content -replace "mavenCentral\(\)", $flatDirRef
-Set-Content -Value $content -Path build.gradle
+if($buildAndDependencyFilesDistinct -eq $false) {
+    $buildContent = $dependenciesContent
+}
+$buildContent = $buildContent -replace "mavenCentral\(\)", $flatDirRef
+Set-Content -Value $buildContent -Path $buildFilePath -Verbose;
+if($buildAndDependencyFilesDistinct -eq $true) {
+    Set-Content -Value $dependenciesContent -Path $dependenciesFilePath -Verbose;
+}
