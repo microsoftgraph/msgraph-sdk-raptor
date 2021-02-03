4594f0457a865604c61587f430d042aa55eada18
Mustafa Zengin <muzengin@microsoft.com>
2021-02-02 17:08:12 -0800

    Add infrastructure to fetch delegated permissions

---
+++ msgraph-sdk-raptor-compiler-lib/Models/Scope.cs
@@ -0,0 +1,11 @@
+﻿namespace MsGraphSDKSnippetsCompiler.Models
+{
+    /// <summary>
+    /// Model from https://github.com/microsoftgraph/microsoft-graph-explorer-api/blob/dev/GraphExplorerPermissionsService/Models/ScopeInformation.cs
+    /// </summary>
+    public class Scope
+    {
+        public string value { get; set; }
+        public bool isAdmin { get; set; }
+    }
+}
