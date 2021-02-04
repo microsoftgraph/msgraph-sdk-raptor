2c7252312b45468b266ec33476dec02da88f4bcc
Mustafa Zengin <muzengin@microsoft.com>
2021-02-04 13:34:41 -0800

    Update DevX API References (#174)

--- msgraph-sdk-raptor-compiler-lib/Models/Scope.cs
+++ msgraph-sdk-raptor-compiler-lib/Models/Scope.cs
@@ -1,7 +1,7 @@
 ﻿namespace MsGraphSDKSnippetsCompiler.Models
 {
     /// <summary>
-    /// Model from https://github.com/microsoftgraph/microsoft-graph-explorer-api/blob/dev/GraphExplorerPermissionsService/Models/ScopeInformation.cs
+    /// Model from https://github.com/microsoftgraph/microsoft-graph-devx-api/blob/dev/GraphExplorerPermissionsService/Models/ScopeInformation.cs
     /// </summary>
     public record Scope (string value, bool isAdmin);
 }
