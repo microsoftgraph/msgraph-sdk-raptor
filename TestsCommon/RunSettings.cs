3d95857ea8d7e90670bcd10b1d6e8d9cecd02e97
Vincent Biret <vibiret@microsoft.com>
2021-03-15 13:50:13 -0400

    - udpates version numbers to match latest java releases

--- TestsCommon/RunSettings.cs
+++ TestsCommon/RunSettings.cs
@@ -24,7 +24,7 @@ namespace TestsCommon
             {
                 if (string.IsNullOrEmpty(_javaLibVersion))
                 {
-                    return Version == Versions.V1 ? "2.3.2" : "0.1.0-SNAPSHOT";
+                    return Version == Versions.V1 ? "3.0.0" : "0.6.0-SNAPSHOT";
                 }
                 else
                 {
