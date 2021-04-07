e5a3e50a9d67a85fb33243daa5e6ab0779979500
Mustafa Zengin <muzengin@microsoft.com>
2021-04-07 13:02:41 -0700

    add comments

--- TestsCommon/IdentifierReplacer.cs
+++ TestsCommon/IdentifierReplacer.cs
@@ -5,7 +5,15 @@ namespace TestsCommon
 {
     public class IdentifierReplacer
     {
+        /// <summary>
+        /// tree of IDs that appear in sample Graph URLs
+        /// </summary>
         private readonly IDTree tree;
+
+        /// <summary>
+        /// regular expression to match strings like {namespace.type-id}
+        /// also extracts namespace.type part separately so that we can use it as a lookup key.
+        /// </summary>
         private readonly Regex idRegex = new Regex(@"{([A-Za-z\.]+)\-id}", RegexOptions.Compiled);
 
         public IdentifierReplacer(IDTree tree)
@@ -13,6 +21,15 @@ namespace TestsCommon
             this.tree = tree;
         }
 
+        /// <summary>
+        /// Replaces ID placeholders of the form {name-id} by looking up name in the IDTree.
+        /// If there are more than one placeholder, it traverses through the tree, e.g.
+        /// For input string "sites/{site-id}/lists/{list-id}",
+        ///   {site-id} is replaced by root->site entry from the IDTree.
+        ///   {list-id} is replaced by root->site->list entry from the IDTree.
+        /// </summary>
+        /// <param name="input">String containing ID placeholders</param>
+        /// <returns>input string, but its placeholders replaced from IDTree</returns>
         public string ReplaceIds(string input)
         {
             if (input == null)
@@ -24,8 +41,8 @@ namespace TestsCommon
             IDTree currentIdNode = tree;
             foreach (Match match in matches)
             {
-                var id = match.Groups[0].Value; // e.g. {application-id}
-                var idType = match.Groups[1].Value; // e.g. extract application from {application-id}
+                var id = match.Groups[0].Value;     // e.g. {site-id}
+                var idType = match.Groups[1].Value; // e.g. extract site from {site-id}
 
                 currentIdNode = currentIdNode[idType];
                 input = input.Replace(id, currentIdNode.Value);
