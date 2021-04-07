d7c32b424742e431cdb8f8f34ec3c8feb1c8f929
Mustafa Zengin <muzengin@microsoft.com>
2021-04-07 12:28:01 -0700

    write IDTree to a file

--- TestsCommon.Tests/IdentiferReplacerTests.cs
+++ TestsCommon.Tests/IdentiferReplacerTests.cs
@@ -1,4 +1,7 @@
 ﻿using NUnit.Framework;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace TestsCommon.Tests
 {
@@ -9,25 +12,42 @@ namespace TestsCommon.Tests
         [SetUp]
         public void Setup()
         {
-            var ids = new IDTree(null)
+            var idRegex = new Regex(@"{([A-Za-z\.]+)\-id}", RegexOptions.Compiled);
+            var lines = System.IO.File.ReadAllLines(@"C:\Users\Admin\OneDrive - Microsoft\Desktop\url-replacement\urlsBefore.txt");
+            var tree = new IDTree(null);
+            foreach (var line in lines.Where(line => line.StartsWith("https:")))
             {
-                ["application"] = new IDTree("<application>"),
-                ["team"] = new IDTree("<team>")
+                var matches = idRegex.Matches(line);
+                var currentIdNode = tree;
+                var parent = string.Empty;
+                foreach (Match match in matches)
                 {
-                    ["channel"] = new IDTree("<team_channel>")
-                    {
-                        ["conversationMember"] = new IDTree("<team_channel_conversationMember>")
-                    }
-                }
-            };
+                    var id = match.Groups[0].Value;
+                    var idType = match.Groups[1].Value;
 
-            idReplacer = new IdentifierReplacer(ids);
+                    parent = parent == string.Empty
+                        ? idType
+                        : $"{parent}_{idType}";
+
+                    if (!currentIdNode.ContainsKey(idType))
+                    {
+                        var subTree = new IDTree($"({parent})");
+                        currentIdNode[idType] = subTree;
+                    }
+
+                    currentIdNode = currentIdNode[idType];
+                }
+            }
+
+            TestContext.Out.WriteLine(JsonSerializer.Serialize(tree, new JsonSerializerOptions { WriteIndented = true }));
+
+            idReplacer = new IdentifierReplacer(tree);
         }
 
         [TestCase("https://graph.microsoft.com/v1.0/applications/{application-id}/owners",
-                  "https://graph.microsoft.com/v1.0/applications/<application>/owners")]
+                  "https://graph.microsoft.com/v1.0/applications/(application)/owners")]
         [TestCase("https://graph.microsoft.com/v1.0/teams/{team-id}/channels/{channel-id}/members/{conversationMember-id}",
-                  "https://graph.microsoft.com/v1.0/teams/<team>/channels/<team_channel>/members/<team_channel_conversationMember>")]
+                  "https://graph.microsoft.com/v1.0/teams/(team)/channels/(team_channel)/members/(team_channel_conversationMember)")]
         public void TestIds(string snippetUrl, string expectedUrl)
         {
             var newUrl = idReplacer.ReplaceIds(snippetUrl);
--- TestsCommon/IDTree.cs
+++ TestsCommon/IDTree.cs
@@ -1,11 +1,36 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace TestsCommon
 {
+    [JsonConverter(typeof(IDTreeConverter))]
     public class IDTree : Dictionary<string, IDTree>
     {
         public string Value { get; set; }
         public IDTree(string value) { Value = value; }
+    }
+
+    public class IDTreeConverter : JsonConverter<IDTree>
+    {
+        public override IDTree Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, IDTree tree, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+            writer.WritePropertyName("value");
+            writer.WriteStringValue(tree.Value);
+            foreach (KeyValuePair<string, IDTree> item in tree)
+            {
+                writer.WritePropertyName(item.Key);
+                Write(writer, item.Value, options);
+            }
+            writer.WriteEndObject();
+        }
     }
 }
 
