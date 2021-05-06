96b2900920f30daa0a271e20116a48b774c94b7a
Mustafa Zengin <muzengin@microsoft.com>
2021-05-06 10:12:45 -0700

    add scope override

--- scripts/readDefaultDataDelegated.ps1
+++ scripts/readDefaultDataDelegated.ps1
@@ -20,17 +20,25 @@ $domain = $appSettings.Username.Split("@")[1]
 function getToken
 {
     param(
-        [string]$path
+        [string]$path,
+        [string]$scopeOverride
     )
 
     $tokenEndpoint = "https://login.microsoftonline.com/$domain/oauth2/v2.0/token"
     $grantType = "password"
     $method = "GET"
 
-    $scopes = Invoke-RestMethod -Method Get -Uri "https://graphexplorerapi.azurewebsites.net/permissions?requesturl=$path&method=$method"
-    $joinedScopeString = $scopes.value |
-        Where-Object { $_.Contains("Read") -and !$_.Contains("Write") } | # same selection as the read-only permissions for the app
-        Join-String -Separator " "
+    if ($scopeOverride)
+    {
+        $joinedScopeString = $scopeOverride
+    }
+    else
+    {
+        $scopes = Invoke-RestMethod -Method Get -Uri "https://graphexplorerapi.azurewebsites.net/permissions?requesturl=$path&method=$method"
+        $joinedScopeString = $scopes.value |
+            Where-Object { $_.Contains("Read") -and !$_.Contains("Write") } | # same selection as the read-only permissions for the app
+            Join-String -Separator " "
+    }
 
     $body = "grant_type=$grantType&username=$($appSettings.Username)&password=$($appSettings.Password)&client_id=$($appSettings.ClientID)&scope=$($joinedScopeString)"
     $token = Invoke-RestMethod -Method Post -Uri $tokenEndpoint -Body $body -ContentType 'application/x-www-form-urlencoded'
@@ -42,10 +50,11 @@ function reqDelegated
 {
     param(
         [string]$version = "v1.0",
-        [string]$url
+        [string]$url,
+        [string]$scopeOverride
     )
 
-    $token = getToken "/$url"
+    $token = getToken -path "/$url" -scopeOverride $scopeOverride
     Connect-MgGraph -AccessToken $token | Write-Host
 
     $response = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/$version/$url" -OutputType PSObject
@@ -57,5 +66,10 @@ $calendarGroup = reqDelegated -url "me/calendarGroups" |
     Select-Object -First 1
 $calendarGroup.id
 $identifiers.calendarGroup._value = $calendarGroup.id
+
+$todoTaskList = reqDelegated -url "me/todo/lists" -scopeOverride "Tasks.Read" |
+    Select-Object -First 1
+$todoTaskList.id
+$identifiers.todoTaskList._value = $todoTaskList.id
 
 $identifiers | ConvertTo-Json -Depth 10 > $identifiersPath
